d0f59427c86cf629a1d73c924751d7e83fde2c23
mukti2216patel <mukti2216patel@gmail.com>
2025-01-26 09:48:59 +0530

    null

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: [],
+  content: [
+    './index.html',          // Path to your index.html file
+    './src/**/*.html',       // If you have other HTML files in the `src` folder, include them too
+    './src/**/*.js',         // If you have JS files that include Tailwind classes, include them as well
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
